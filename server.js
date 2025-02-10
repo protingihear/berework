8da67c468dc42a373688e95fa1e381b14ed27afe
fharlup <fajarmufid01@gmail.com>
2025-02-10 23:35:27 +0700

    plis dong jan crash

--- server.js
+++ server.js
@@ -4,27 +4,36 @@ const cors = require('cors');
 const sequelize = require('./config/database');
 const AuthRoutes = require('./routes/AuthRoutes');
 const UserRoutes = require('./routes/UserRoutes');
+const MySQLStore = require('express-mysql-session')(session);
 
 require('dotenv').config();
 
 const app = express();
 
+const dbOptions = {
+    host: process.env.MYSQLHOST || 'localhost',
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || '',
+    database: process.env.MYSQL_DATABASE || 'your_database'
+};
+const sessionStore = new MySQLStore(dbOptions);
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: '*'
 }));
+
 app.use(session({
-    secret: process.env.SECRET_KEY,
+    key: 'tt',
+    secret: 'tt', // Ganti dengan secret key yang aman
+    store: sessionStore,
     resave: false,
     saveUninitialized: false,
     cookie: {
-      httpOnly: true,
-      secure: false, //ini cookie nya gua gangerti deh nanti mo di nyalain atau matiin
-      maxAge: 1000 * 60 * 60 * 24, // 1 hari
-    },
+        secure: process.env.NODE_ENV === 'production', // Hanya aktif di production jika pakai HTTPS
+        maxAge: 1000 * 60 * 60 * 24 // 1 hari
+    }
 }));
-
 app.use('/auth', AuthRoutes);
 app.use('/api', UserRoutes);
 
