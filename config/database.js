25b75af09059f7350621977e7b5287abb444bf09
fharlup <fajarmufid01@gmail.com>
2025-02-10 23:24:55 +0700

    push

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE="reworkdb"
+, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+    host: process.env.MYSQLHOST,
     dialect: 'mysql',
     logging: false,
 });
