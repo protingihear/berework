d22061f7535f35886dfa870eafaaba03cbfae6d9
fharlup <fajarmufid01@gmail.com>
2025-02-10 23:50:21 +0700

    tolong jan crash lagi udah ngantuk

--- config/database.js
+++ config/database.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE="reworkdb"
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE
 , process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+     port: process.env.DB_PORT || 3306,
     host: process.env.MYSQLHOST,
     dialect: 'mysql',
     logging: false,
+    
 });
 
 module.exports = sequelize;
