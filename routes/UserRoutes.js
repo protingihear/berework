88904939030c0a773e6d7a1f648d09516bec7149
fharlup <fajarmufid01@gmail.com>
2025-02-22 16:32:54 +0700

    fix kategori

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,7 +3,6 @@ const UserController = require('../controllers/UserController');
 const authMiddleware = require('../middleware/authMiddleware');
 const upload = require('../middleware/upload');
 
-const upload = require("../middleware/upload");
 const router = express.Router();
 
 router.get('/users', authMiddleware, UserController.getAllUsers);
