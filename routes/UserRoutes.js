bf29d6024930d7c29c2a5f3f09d83036df27fc5c
fharlup <fajarmufid01@gmail.com>
2025-02-22 15:50:22 +0700

    fix minor

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const UserController = require('../controllers/UserController');
 const authMiddleware = require('../middleware/authMiddleware');
 
+const upload = require("../middleware/upload");
 const router = express.Router();
 
 router.get('/users', authMiddleware, UserController.getAllUsers);
 router.get('/profile', authMiddleware, UserController.getUserProfile);
-router.put('/profile', authMiddleware, UserController.updateUser);
+router.put('/profile',upload.single(Image), authMiddleware, UserController.updateUser);
 router.delete('/profile', authMiddleware, UserController.deleteUser);
 router.post('/logout', authMiddleware, UserController.logout);
 
