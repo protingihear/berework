08a30fbc8e7d4b04d7b18c1921c3245a6199366b
ArgaAAL <109477340+ArgaAAL@users.noreply.github.com>
2025-02-24 16:26:21 +0700

    Update server.jsasas

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production' ? true : false, // Set to false for localhost
+        secure:  false, // Set to false for localhost
         httpOnly: false,
         sameSite: 'lax', // Allows cross-origin cookies in most cases
         maxAge: 1000 * 60 * 60 * 24 // 1 day
