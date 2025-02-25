b2590368b5c2e9534fa69c4153bbcd1d60496abf
ArgaAAL <argalumunon9@gmail.com>
2025-02-25 09:17:27 +0700

    fix erorr pajar

--- models/CommunityPost.js
+++ models/CommunityPost.js
@@ -3,7 +3,7 @@ const sequelize = require("../config/database");
 
 const CommunityReply = require("./CommunityReply");
 const CommunityLike = require("./CommunityLike");
-const User = require("./user");
+// const User = require("./user");
 const CommunityPost = sequelize.define("CommunityPost", {
     id: {
         type: DataTypes.INTEGER,
@@ -26,9 +26,5 @@ const CommunityPost = sequelize.define("CommunityPost", {
 
 // 🔹 Hubungan dengan Reply
 CommunityPost.hasMany(CommunityReply, { foreignKey: "postId", as: "replies" });
-CommunityReply.belongsTo(CommunityPost, { foreignKey: "postId", as: "post" });
-
-// 🔹 Hubungan dengan Like
 CommunityPost.hasMany(CommunityLike, { foreignKey: "postId", as: "likes" });
-CommunityPost.belongsTo(User, { foreignKey: "userId", as: "author" });
 module.exports = CommunityPost;
