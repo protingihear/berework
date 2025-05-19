9d1358b5112c6e0ba4925b78ae65e8555d25d6b5
andamiegelas <fharlup@gmail.com>
2025-05-20 06:30:16 +0700

    fix gambar post3

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const storage = multer.memoryStorage(); 
 const upload = multer({ 
     storage: storage,
-    limits: { fileSize: 2 * 1024 * 1024 } // Batas maksimum 2MB
+    limits: { fileSize: 10 * 1024 * 1024 } // Batas maksimum 2MB
 });
 
 module.exports = upload;
